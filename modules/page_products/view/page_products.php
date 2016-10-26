b00993edcc45b7190db0ac06552bd507cd636de6
Jorge <jordimart83@gmail.com>
2016-10-27 00:02:54 +0200

    He creado el modulo page_products y he comprobado que cargue la vista page_products

---
+++ modules/page_products/view/page_products.php
@@ -0,0 +1,41 @@
+<script type="text/javascript" src="modules/page_products/view/js/jquery.bootpag.min.js"></script>
+<script type="text/javascript" src="modules/page_products/view/js/page_products.js" ></script>
+<div id="results"></div>
+
+<center>
+    <div class="pagination"></div>
+    <h1>Ahora estaria en pagination</h1>
+</center>
+
+<!-- modal window details_product -->
+<section id="product">
+
+    <div id="details_prod" hidden>
+
+        <!--<ol class="breadcrumb">
+            <li><a href="index.php?module=products">Products</a></li>
+            <li class="active">Details Product</li>
+        </ol>
+        <br>
+        <br>-->
+        <div id="details">
+            <div id="img_prod" class="prodImg"></div>
+
+            <div id="container">
+
+                <h4> <strong><div id="name_prod"></div></strong> </h4>
+                <br />
+                <p>
+                <div id="description_prod"></div>
+                </p>
+                <p>
+                <div id="titration_prod"></div>
+                </p>
+                <h2> <strong><div id="price_prod"></div></strong> </h5>
+
+            </div>
+
+        </div>
+
+    </div>
+</section>
--- view/inc/menu.php
+++ view/inc/menu.php
@@ -6,13 +6,25 @@
                         echo "active";}else{ echo "";} ?> >
                         <a href="index.php?module=main">Home</a>
                       </li>
-                 <li class= <?php if($_GET['module'] == "products"){
-                        echo "active";}else{ echo "";} ?> >
-                        <a href="index.php?module=products&view=create_products">Products</a>
-                      </li>
+
+              <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Products Frontend<span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li class= <?php if($_GET['module'] == "products"){
+                           echo "active";}else{ echo "";} ?> >
+                           <a href="index.php?module=products&view=create_products">Create Products</a>
+                    </li>
+                    <li class= <?php if($_GET['module'] == "page_products"){
+                           echo "active";}else{ echo "";} ?> >
+                           <a href="index.php?module=page_products&view=page_products">List Products</a>
+                    </li>
+
+                  </ul>
+                </li>
+
                   <li class= <?php if($_GET['module'] == "products"){
                           echo "active";}else{ echo "";} ?> >
-                          <a href="index.php?module=products">List_products</a>
+                          <a href="index.php?module=products">List products php</a>
                         </li>
                 <li class= <?php if($_GET['module'] == "contact"){
                         echo "active";}else{ echo "";} ?> >
